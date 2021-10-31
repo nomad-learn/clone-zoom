d17d5aec819803fdcfd971fe274a0974061bb6f1
cuttleman <silluat11@gmail.com>
2021-11-01 08:37:26 +0900

    senders yet

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,17 +2,37 @@ const video = document.getElementById("myFace");
 const audioBtn = document.getElementById("audio");
 const cameraBtn = document.getElementById("camera");
 const select = document.getElementById("select");
+const form = document.getElementById("join");
+const videoContainer = document.getElementById("myStream");
+const roomNameEl = document.getElementById("roomName");
+const notification = document.getElementById("notification");
+
+const socket = io();
 
 let myStream;
 let isMuted = false;
 let isCameraOff = false;
+let isEnter = false;
+let myPeerConnection;
+let room;
+
+form.hidden = false;
+videoContainer.hidden = true;
+
+const createNofi = (msg) => {
+  notification.innerText = msg;
+  notification.classList.toggle("show");
+  setTimeout(() => {
+    notification.classList.toggle("show");
+  }, 2000);
+};
 
 const getCamera = async () => {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
     const currentCamera = myStream.getVideoTracks()[0];
-    console.log(currentCamera);
+
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
@@ -29,8 +49,8 @@ const getCamera = async () => {
 
 async function getMedia(deviceId) {
   const initialConstrains = {
-    audio: true,
-    video: { facingMode: "user" },
+    audio: false,
+    video: true,
   };
   const cameraConstrains = {
     audio: true,
@@ -49,7 +69,12 @@ async function getMedia(deviceId) {
   }
 }
 
-getMedia();
+const startMedia = async () => {
+  form.hidden = true;
+  videoContainer.hidden = false;
+  await getMedia();
+  makeConnection();
+};
 
 const handleMuteClick = () => {
   myStream
@@ -81,6 +106,67 @@ const handleCameraSwitch = async () => {
   getMedia(select.value);
 };
 
+const handleJoin = async (e) => {
+  e.preventDefault();
+  const input = form.querySelector("input");
+  const value = input.value;
+  await startMedia();
+  socket.emit("join", value);
+  room = value;
+  roomNameEl.innerText = value;
+  input.value = "";
+};
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("track", handleAddTrack);
+  myStream.getTracks().forEach((track) => {
+    myPeerConnection.addTrack(track, myStream);
+  });
+}
+
+function handleIce(data) {
+  socket.emit("ice", data.candidate, room);
+}
+function handleAddTrack(data) {
+  console.log(data.streams[0]);
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.streams[0];
+}
+
 audioBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 select.addEventListener("input", handleCameraSwitch);
+form.addEventListener("submit", handleJoin);
+
+// my browser
+socket.on("hello", async (msg) => {
+  createNofi(msg);
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  socket.emit("offer", offer, room);
+});
+
+socket.on("bye", (msg) => {
+  createNofi(msg);
+});
+
+// another browser
+socket.on("offer", async (offer) => {
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, room);
+});
+
+// my browser
+socket.on("answer", (answer) => {
+  myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", (ice) => {
+  myPeerConnection.addIceCandidate(ice);
+});
+
+getMedia();
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import bcrypt from "bcrypt";
 
-const PORT = 4000;
+const PORT = 3000;
 
 const app = express();
 
@@ -26,6 +26,28 @@ const io = new Server(server, {
     origin: ["https://admin.socket.io"],
     credentials: true,
   },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join", async (msg) => {
+    const roomName = msg;
+    socket.join(roomName);
+    socket.to(roomName).emit("hello", "someone joined");
+  });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => {
+      io.to(room).emit("bye", "someone out");
+    });
+  });
+  socket.on("offer", (offer, room) => {
+    socket.to(room).emit("offer", offer);
+  });
+  socket.on("answer", (answer, room) => {
+    socket.to(room).emit("answer", answer);
+  });
+  socket.on("ice", (ice, room) => {
+    socket.to(room).emit("ice", ice);
+  });
 });
 
 server.listen(PORT, handler);
